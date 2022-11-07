0ba642a4b67c3ffe10c782f6196cd1987dc5ed53
woobong1 <111822994+woobong1@users.noreply.github.com>
2022-11-07 14:42:59 +0900

    what happened?
    I don't know

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
 
 public class GameManager : MonoBehaviour
 {
@@ -30,6 +31,69 @@ public class GameManager : MonoBehaviour
     public Vector3 GetPlayerPos(int num)
     {
         return playerPos[num];
+=======
+using Photon.Pun;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int unitCount;
+    private int[] playerSetPos;
+    Vector3[] startPos;
+    Vector3 playerStartPos;
+    PhotonView PV;
+    
+
+    private void Awake()
+    {
+        startPos = new Vector3[unitCount];
+        playerSetPos = new int[4];
+
+        if (PV.IsMine)
+        {
+            playerStartPos.x = Random.Range(-120, 120);
+            playerStartPos.z = Random.Range(-120, 120);
+            playerStartPos.y = 1;
+            PhotonNetwork.Instantiate("Player", playerStartPos, Quaternion.identity);
+        }
+
+        SetStartPos(); // AI 
+    }
+
+    private void Start()
+    {
+        
+
+        for (int i = 0; i < unitCount; i++) // AI Instance
+        {
+            GameObject inst = PhotonNetwork.Instantiate("Unit", startPos[i], Quaternion.identity);
+        }
+    }
+
+
+    public void SetStartPos()
+    {
+        for (int i = 0; i < startPos.Length; i++)
+        {
+            startPos[i].x = Random.Range(-120, 120);
+            startPos[i].z = Random.Range(-120, 120);
+            // if pos as same as playerPos
+            if (Mathf.Abs(startPos[i].x - playerStartPos.x) < 1 && Mathf.Abs(startPos[i].z - playerStartPos.z) < 1)
+            {
+                --i;
+                continue;
+            }
+            startPos[i].y = 1;
+
+            for (int j = 0; j < i; j++)
+            {
+                while (Mathf.Abs(startPos[i].x - startPos[j].x) < 1 && Mathf.Abs(startPos[i].z - startPos[j].z) < 1)
+                {
+                    startPos[i].x = Random.Range(-120, 120);
+                    startPos[i].z = Random.Range(-120, 120);
+                }
+            }
+        }
+>>>>>>> Stashed changes
     }
 
 
