2b2640d1a41d1fc74b9190be3049c14e42d20e52
MUHYUK JEON <67819713+JEONMUHYUK@users.noreply.github.com>
2022-11-07 10:26:29 +0900

    Create StartScene && LobbyScene
    Create StartScene && LobbyScene

---
+++ Assets/Photon/Scripts/PhotonManager.cs
@@ -0,0 +1,29 @@
+using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private TextMeshProUGUI nikName = null;
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("#############OnConnectedToMaster");
+        nikName.text = PhotonNetwork.NickName;
+    }
+
+    private void OnGUI()
+    {
+        GUIStyle gUIStyle = new GUIStyle(GUI.skin.label);
+        gUIStyle.fontSize = 20;
+        gUIStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(0f, 0f, 350f, 50f), "ServeState : " + PhotonNetwork.Server.ToString(), gUIStyle);
+    }
+}
