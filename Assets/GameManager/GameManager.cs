506fffd08301dc3c6df99082484f716ccde04691
woobong1 <111822994+woobong1@users.noreply.github.com>
2022-11-07 16:36:08 +0900

    45

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -3,56 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPun
 {
     [SerializeField] int unitCount;
-    private int[] playerSetPos;
+    private Vector3[] playerSetPos;
     Vector3[] startPos;
-    Vector3 playerStartPos;
     PhotonView PV;
     
 
     private void Awake()
     {
         startPos = new Vector3[unitCount];
-        playerSetPos = new int[4];
+        playerSetPos = new Vector3[4];
 
-        if (PV.IsMine)
-        {
-            playerStartPos.x = Random.Range(-120, 120);
-            playerStartPos.z = Random.Range(-120, 120);
-            playerStartPos.y = 1;
-            PhotonNetwork.Instantiate("Player", playerStartPos, Quaternion.identity);
-        }
+
+            PhotonNetwork.Instantiate("Player", playerSetPos[0], Quaternion.identity);
+        
+       
+
 
         SetStartPos(); // AI 
     }
 
     private void Start()
     {
-        
-
-        for (int i = 0; i < unitCount; i++) // AI Instance
+        if (PhotonNetwork.IsMasterClient) // AI add only masterClient
         {
-            GameObject inst = PhotonNetwork.Instantiate("Unit", startPos[i], Quaternion.identity);
+            for (int i = 0; i < unitCount; i++) // AI Instance
+            {
+                GameObject inst = PhotonNetwork.Instantiate("Unit", startPos[i], Quaternion.identity);
+            }
         }
     }
 
+    public void SetPlayerStartPos()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            playerSetPos[i].x = Random.Range(-120, 120);
+            playerSetPos[i].z = Random.Range(-120, 120);
+            for(int j = 0; j < i; j++)
+            {
+                if (i != j && Mathf.Abs(playerSetPos[i].x - playerSetPos[j].x) < 1 && Mathf.Abs(playerSetPos[i].z - playerSetPos[j].z) < 1)
+                {
+                    while (Mathf.Abs(playerSetPos[i].x - playerSetPos[j].x) < 1 && Mathf.Abs(playerSetPos[i].z - playerSetPos[j].z) < 1)
+                    {
+                        playerSetPos[i].x = Random.Range(-120, 120);
+                        playerSetPos[i].z = Random.Range(-120, 120);
+                    }
+                }
+            }
+            
+            playerSetPos[i].y = 1;
+
+
+        }
+    }
 
     public void SetStartPos()
     {
         for (int i = 0; i < startPos.Length; i++)
         {
-            startPos[i].x = Random.Range(-120, 120);
-            startPos[i].z = Random.Range(-120, 120);
             // if pos as same as playerPos
-            if (Mathf.Abs(startPos[i].x - playerStartPos.x) < 1 && Mathf.Abs(startPos[i].z - playerStartPos.z) < 1)
+            for(int j = 0; j < i; j++)
             {
-                --i;
-                continue;
+                while(Mathf.Abs(startPos[i].x - playerSetPos[j].x) < 1 && Mathf.Abs(startPos[i].z - playerSetPos[j].z) < 1)
+                {
+                    startPos[i].x = Random.Range(-120, 120);
+                    startPos[i].z = Random.Range(-120, 120);
+                }
             }
+            
             startPos[i].y = 1;
 
+            // if pos as same as other Unit pos
             for (int j = 0; j < i; j++)
             {
                 while (Mathf.Abs(startPos[i].x - startPos[j].x) < 1 && Mathf.Abs(startPos[i].z - startPos[j].z) < 1)
